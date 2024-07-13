e1cdc87a3c8b0477d089258aabfb8a8eac17bc9a
Lokujumaurice <lokujumaurice02@gmail.com>
2024-07-13 14:46:46 +0300

    first python code

---
+++ hello.py
@@ -0,0 +1,9 @@
+def factoral(n):
+    if n = = 0:
+    else:
+        return n *factoral(n - 1)
+        
+  
+  number = 5
+  result = factoral(number)
+  print(f"The factoral oof {number} is {result}. ")
